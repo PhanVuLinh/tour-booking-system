6417a5851f6267dee8459ab55aabf342045fbe67
PhanVuLinh <linhphanzzz04@gmail.com>
2026-06-01 01:35:47 +0700

    Header , footer ,box-contact

--- frontend/react-app/src/layouts/client/MainLayout.jsx
+++ frontend/react-app/src/layouts/client/MainLayout.jsx
@@ -1,10 +1,22 @@
 import { Outlet } from "react-router-dom";
+import "../../assets/Client/css/style.css";
+
+import Header from "../../components/client/Header";
+import Footer from "../../components/client/Footer";
+import BoxContact from "../../components/client/BoxContact";
 
 function MainLayout() {
   return (
     <>
-      Layout
-      <Outlet />
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+
+      <BoxContact />
     </>
   );
 }
