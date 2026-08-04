4a31bb53e2df31c90629f8343f60af4b2fc9eb32
PhanVuLinh <linhphanzzz04@gmail.com>
2026-08-05 00:19:43 +0700

    refactor: Chuẩn hóa đường dẫn API auth có tiền tố /auth cho cả Frontend và Backend

--- frontend/react-app/src/client/modules/auth/services/authService.js
+++ frontend/react-app/src/client/modules/auth/services/authService.js
@@ -1,21 +1,21 @@
 import { post } from "../../../utils/request";
 
 export const register = async (userData) => {
-  const result = await post("/register", userData);
+  const result = await post("/auth/register", userData);
   return result;
 };
 
 export const login = async (loginData) => {
-  const result = await post("/login", loginData);
+  const result = await post("/auth/login", loginData);
   return result;
 };
 
 export const loginGoogle = async (idToken) => {
-  const result = await post("/login/google", { idToken });
+  const result = await post("/auth/login/google", { idToken });
   return result;
 };
 
 export const loginFacebook = async (accessToken) => {
-  const result = await post("/login/facebook", { accessToken: accessToken });
+  const result = await post("/auth/login/facebook", { accessToken: accessToken });
   return result;
 };
