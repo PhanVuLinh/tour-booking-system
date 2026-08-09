ee012b999eb4fba81d5d9cd29e7e0972bfbaa7a6
Đỗ Văn Hin <dovanhin04@gmail.com>
2026-08-09 16:16:55 +0700

    [HinDV]- BE - Cors

--- backend/java/src/main/java/com/lvtn/java/config/CorsConfig.java
+++ backend/java/src/main/java/com/lvtn/java/config/CorsConfig.java
@@ -7,13 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Override 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "http://localhost:3000"
+                        "http://localhost:3000",
+                        "https://tralvelgo.vercel.app"
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
